76fa8a09df83e95bb9e4e64b2df997411556aa46
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2015-08-19 05:48:37 +0700

    Intro (PHP di dalam HTML)

---
+++ Web Programming/PHP/html_php.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Belajar PHP</title>
+<head>
+<body>
+
+<?php
+	//Komentar
+	echo "Komentar</br>";
+	/*
+		Komentar
+		Komentar
+	*/
+	#Komentar
+	$a = "Belajar PHP";
+	echo $a." di rumah sendirian."; //Tanda titik berfungsi untuk menggabungkan 2 statement
+?>
+
+</body>
+</html>
