470ab63a4e8ea4b7d4d4e193ae06db815403fc45
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2015-08-19 06:08:25 +0700

    Variabel dalam PHP

---
+++ Web Programming/PHP/variabel.php
@@ -0,0 +1,10 @@
+<?php
+	$variabel_1 = 7; //Contoh deklarasi variabel
+	$variabel_2 = 8;
+	$jumlah = $variabel_1 + $variabel_2;
+	echo "Variabel ke-1 berisi ".$variabel_1."</br>";
+	echo "Variabel ke-2 berisi ".$variabel_2."</br>";
+	//Mari kita jumlahkan
+	echo $variabel_1." + ".$variabel_2." = ";
+	echo $jumlah;
+?>
