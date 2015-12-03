707c15085db6e932135b57c137b04daeb36b2183
Ifan Faizal Adnan <ifanfaizaladnan@gmail.com>
2015-12-03 21:01:23 +0700

    Update DiamondString2.java

--- Java/DiamondString2.java
+++ Java/DiamondString2.java
@@ -12,24 +12,16 @@ public class DiamondString2 {
         if (kalimat.length()%2==0){   
             kalimat += " ";
             for (int i=0;i<kalimat.length();i++){ 
-                //System.out.println(kalimatbaru);
                 if (i==kalimat.length()-1){           
                     kalimatbaru += kalimat.substring(i, i+1);
                 }else{
                     kalimatbaru += kalimat.substring(i, i+1)+" ";  
                 }
             }  
-            //System.out.println(kalimatbaru);
-            //System.out.println(kalimatbaru.length());
 
             int penambah = kalimatbaru.length()/2;
             int penambah1 = 1;
-            // J a v a   M u d a h   S e k a l i || 33
-            // 012345678901234567890123456789012
-            /*
-            System.out.print(kalimatbaru.substring(0+16, 0+16+1));
-            System.out.print(kalimatbaru.substring(1+13, 1+13+5));
-            */
+           
             for (int i=0,k=0,l=0;i<=kalimat.length()/2;i++,l+=2){
                 for (int j=kalimatbaru.length()/2;j>l;j--){
                     System.out.print(" ");
@@ -44,14 +36,7 @@ public class DiamondString2 {
                 }
                 System.out.println();            
             }
-            // J a v a   M u d a h   S e k a l i || 33
-            // 012345678901234567890123456789012
-            /*
-            System.out.print(kalimatbaru.substring(16-14, 16-14+29));
-            System.out.print(kalimatbaru.substring(14-10, 14-10+25));
-            System.out.print(kalimatbaru.substring(12-6, 12-6+19));
-            System.out.print(kalimatbaru.substring(10-2, 10-2+14));
-            */
+            
             int pengurang = kalimatbaru.length()/2-2;
             int pengurang1 = kalimatbaru.length()-4;
             for (int i=1,k=kalimatbaru.length()/2,l=2;i<=kalimat.length()/2;i++,l+=2){
@@ -68,24 +53,16 @@ public class DiamondString2 {
             }
         }else{
             for (int i=0;i<kalimat.length();i++){ 
-                //System.out.println(kalimatbaru);
                 if (i==kalimat.length()-1){           
                     kalimatbaru += kalimat.substring(i, i+1);
                 }else{
                     kalimatbaru += kalimat.substring(i, i+1)+" ";  
                 }
             }  
-            //System.out.println(kalimatbaru);
-            //System.out.println(kalimatbaru.length());
 
             int penambah = kalimatbaru.length()/2;
             int penambah1 = 1;
-            // J a v a   M u d a h   S e k a l i || 33
-            // 012345678901234567890123456789012
-            /*
-            System.out.print(kalimatbaru.substring(0+16, 0+16+1));
-            System.out.print(kalimatbaru.substring(1+13, 1+13+5));
-            */
+           
             for (int i=0,k=0,l=0;i<=kalimat.length()/2;i++,l+=2){
                 for (int j=kalimatbaru.length()/2;j>l;j--){
                     System.out.print(" ");
@@ -100,14 +77,7 @@ public class DiamondString2 {
                 }
                 System.out.println();            
             }
-            // J a v a   M u d a h   S e k a l i || 33
-            // 012345678901234567890123456789012
-            /*
-            System.out.print(kalimatbaru.substring(16-14, 16-14+29));
-            System.out.print(kalimatbaru.substring(14-10, 14-10+25));
-            System.out.print(kalimatbaru.substring(12-6, 12-6+19));
-            System.out.print(kalimatbaru.substring(10-2, 10-2+14));
-            */
+            
             int pengurang = kalimatbaru.length()/2-2;
             int pengurang1 = kalimatbaru.length()-4;
             for (int i=1,k=kalimatbaru.length()/2,l=2;i<=kalimat.length()/2;i++,l+=2){
